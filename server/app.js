ab33a1db650045ddbc27ded17346802b7e7a78a5
Jaydonxu <xhj6518@163.com>
2023-01-31 19:15:32 +0800

    feat:express

--- server/app.js
+++ server/app.js
@@ -4,8 +4,6 @@ const express = require('express')
 // 解决跨域中间件
 const cors = require('cors')
 
-const db = require('./db/index')
-
 // express——user路由
 const userRouter = require('./router/user')
 
@@ -13,10 +11,22 @@ const userRouter = require('./router/user')
 const app = express()
 app.use(express.static('dist'))
 app.use(cors())
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(userRouter)
+
+// 统一处理返回结果
+app.use((err, req, res, next) => {
+  console.log(req.que, 'req')
+
+  res.send({
+    success: false,
+    error_message: 'shibai'
+  })
+})
+
 // 开启监听端口号
 app.listen(3000, () => {
   console.log('http://localhost:3000')
