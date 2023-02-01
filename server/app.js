9fc9ebe703c1e67776758a0c1c314aaf06c65b97
Jaydonxu <xhj6518@163.com>
2023-02-01 10:03:45 +0800

    test:测试中间件

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 // express——user路由
 const userRouter = require('./router/user')
 
+const resultHandle = require('./utils/result')
+
 // 创建express实例
 const app = express()
 app.use(express.static('dist'))
@@ -18,14 +20,16 @@ app.use(express.json())
 app.use(userRouter)
 
 // 统一处理返回结果
-app.use((err, req, res, next) => {
-  console.log(req.que, 'req')
+app.use('/', (req, res, next) => {
+  console.log(req.err, 'req')
 
   res.send({
     success: false,
     error_message: 'shibai'
   })
 })
+
+// app.use(resultHandle)
 
 // 开启监听端口号
 app.listen(3000, () => {
