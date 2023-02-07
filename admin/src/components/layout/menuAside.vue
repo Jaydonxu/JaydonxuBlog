90f99a00b985beb3e62e6df87df14a617188e40f
Jaydonxu <xhj6518@163.com>
2023-02-08 00:39:57 +0800

    change:菜单栏抽离

---
+++ admin/src/components/layout/menuAside.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="aside">菜单栏</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.aside {
+  height: 100%;
+  background-color: #2f3e52;
+  color: #fff;
+}
+</style>
