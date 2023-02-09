b77163dd3a89d85a247c7e4cf5e049a418118067
Jaydonxu <xhj6518@163.com>
2023-02-10 01:03:48 +0800

    fete:新增页面

---
+++ admin/src/views/404/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>抱歉，未找到该页面！</h3>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- admin/src/views/bulletin/index.vue
+++ admin/src/views/bulletin/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <!-- 个人中心 -->
-  <div>这是user页面</div>
+  <div>这是bulletin页面</div>
 </template>
 
 <style scoped></style>
