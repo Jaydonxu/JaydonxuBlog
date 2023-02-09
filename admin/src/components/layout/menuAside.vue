96f1bfa3c3df997e2ba0759a940af75edb91be31
Jaydonxu <xhj6518@163.com>
2023-02-10 01:03:29 +0800

    update:菜单栏

--- admin/src/components/layout/menuAside.vue
+++ admin/src/components/layout/menuAside.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="aside">菜单栏</div>
+  <div class="aside">
+    <ul class="menu-content">
+      <li
+        v-for="item in routeList"
+        class="menu-item"
+        @click="$router.push(`${item.path}`)"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "../../router/index";
+import { ref, reactive } from "vue";
+console.log(routes);
 
-<style scoped>
+const _routes = routes.filter((item) => !item.hidden);
+
+const routeList = reactive({
+  ..._routes,
+});
+console.log(routeList, "routeListrouteListrouteList");
+</script>
+
+<style scoped lang="scss">
 .aside {
   height: 100%;
   background-color: #2f3e52;
   color: #fff;
+  .menu-content {
+    .menu-item {
+      text-align: center;
+      height: 60px;
+      line-height: 60px;
+      font-size: 20px;
+      cursor: pointer;
+      &:hover {
+        background-color: rgba(36, 50, 69, 0.5);
+      }
+    }
+  }
 }
 </style>
