1bc5a898b0f2a226e609a84d1331571389555fc2
Jaydonxu <xhj6518@163.com>
2023-02-13 23:53:24 +0800

    feat:退出登录

--- admin/src/components/layout/header.vue
+++ admin/src/components/layout/header.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="head-container flex flex-end items-center">
-    <span class="user-img">
-      <img src="../../assets/imgs/home/userImg.jpeg" alt />
-    </span>
+    <el-dropdown>
+      <span class="user-img">
+        <img src="../../assets/imgs/home/userImg.jpeg" />
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item
+            @click="
+              () => {
+                $router.push('/user');
+              }
+            "
+            >主页</el-dropdown-item
+          >
+          <el-dropdown-item @click="loginOut">退出登录</el-dropdown-item>
+          <!-- <el-dropdown-item>Action 3</el-dropdown-item> -->
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function loginOut() {
+  console.log("执行了");
+
+  window.localStorage.removeItem("token");
+}
+</script>
 
 <style lang="scss" scoped>
 .head-container {
