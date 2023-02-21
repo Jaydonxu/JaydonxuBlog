4c57d57b7fb374743ad4c575122c06b171e7d3b4
Jaydonxu <xhj6518@163.com>
2023-02-22 00:51:53 +0800

    update:数字滚动优化

--- admin/src/components/scrollNumber/index.vue
+++ admin/src/components/scrollNumber/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="flex items-center justify-center test-container">
-    <div class="number" v-for="item, index in numList" :key="index">
-      <span class="list" v-if="item === ',' || item === '.'">{{ item }}</span>
-      <span class="list" v-else :ref="getList">0123456789</span>
+    <div
+      class="number"
+      :style="{ ...numStyle }"
+      v-for="(item, index) in numList"
+      :key="index"
+    >
+      <span
+        class="list"
+        :style="{
+          fontSize: numStyle.height,
+        }"
+        :ref="getList"
+        >0123456789</span
+      >
     </div>
   </div>
   <div>
@@ -12,52 +23,47 @@
 
 <script setup lang="ts">
 import { ref, defineProps, onMounted } from "vue";
-// const list = ref([1, 2, 4, 5]);
 const props = defineProps({
   number: {
     type: String,
-    default: "1,234,567,894,567.89",
+    default: "123456789456789",
   },
   numStyle: {
     type: Object,
     default: {
-      width: "14px",
-      height: "14px",
-      backgroundColor: 'red',
-    }
+      width: "20px",
+      height: "20px",
+      backgroundColor: "rgb(255, 255, 255, 0.4)",
+      marginRight: "4px",
+      fontSize: "14px",
+      borderRadius: "2px",
+      display: "block",
+    },
   },
-
 });
+console.log(parseInt(props.numStyle.height), "numStylenumStylenumStyle");
 
-const strList = ref(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
-console.log(strList);
 const numList = ref(props.number.split(""));
-
-const listDom = []
+const listDom = [];
 const getList = (el) => {
-  listDom.push(el)
-}
+  listDom.push(el);
+};
 onMounted(() => {
   Array.from(listDom).forEach((c, i) => {
-    const isNum = numList.value[i] !== ',' && numList.value[i] !== '.'
-    const timerObj = {}
-    let index = 0
+    const isNum = numList.value[i] !== "," && numList.value[i] !== ".";
+    const timerObj = {};
+    let index = 0;
     timerObj[i] = setInterval(() => {
-      index++
+      index++;
       if (index.toString() !== numList.value[i] && isNum) {
-        c.style.transform = `translateY(-${index * 16}px)`
+        c.style.transform = `translateY(-${index * 10}%)`;
       } else {
-        clearInterval(timerObj[i])
-        timerObj[i] = null
-        // if (!isNum) {
-        //   c.innerHTML = numList.value[i]
-        //   console.dir(c, "改变值：：");
-
-        // }
+        clearInterval(timerObj[i]);
+        timerObj[i] = null;
       }
-    }, 100)
-  })
-})
+    }, 100);
+  });
+});
 </script>
 
 <style lang="scss" scoped>
@@ -66,9 +72,8 @@ onMounted(() => {
   height: 100vh;
 
   .number {
-    width: 20px;
-    height: 20px;
-    // border: 1px solid #333;
+    display: block;
+    border: 1px solid red;
     overflow: hidden;
 
     span {
@@ -80,6 +85,7 @@ onMounted(() => {
     }
 
     .list {
+      box-sizing: border-box;
       // border: 1px solid red;
     }
   }
