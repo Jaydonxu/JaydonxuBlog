271b05b0ac251f03618c1e51cb57a95543b769a7
Jaydonxu <xhj6518@163.com>
2023-02-26 22:57:25 +0800

    update:优化首页结构

--- admin/src/views/home/index.vue
+++ admin/src/views/home/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts"></script>
 <template>
-  <div>这是home页面</div>
+  <div class="home-container">
+    <div class="home-user-info">个人信息</div>
+    <div class="user-blog-data">博客数据</div>
+    <div class="blog-date">当日时间</div>
+  </div>
 </template>
 
 <style scoped></style>
