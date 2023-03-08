6604ff85252457c84e7f29868130a3c6248326ab
Jaydonxu <xhj6518@163.com>
2023-03-08 23:55:25 +0800

    feat:导入mavon-editor插件

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -7,11 +7,13 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "./assets/style/globel.css";
 import "./assets/style/common.css";
-
+import mavonEditor from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
 import App from "./App.vue";
 
 const app = createApp(App);
 
+app.use(mavonEditor);
 app.use(ElementPlus);
 app.use(router);
 app.mount("#app");
--- admin/src/views/blog/index.vue
+++ admin/src/views/blog/index.vue
@@ -1,6 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const value = ref("");
+console.log(value, "value");
+</script>
 <template>
-  <div>博客列表==> 导入md依赖</div>
+  <div>
+    <mavon-editor v-model="value" />
+  </div>
 </template>
 
 <style scoped></style>
