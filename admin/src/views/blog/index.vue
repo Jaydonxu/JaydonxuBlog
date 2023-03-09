bba40237b5569d8d659d0481bfc0267e4c653350
Jaydonxu <xhj6518@163.com>
2023-03-09 23:55:39 +0800

    fix:保存文件事件

--- admin/src/views/blog/index.vue
+++ admin/src/views/blog/index.vue
@@ -2,10 +2,14 @@
 import { ref } from "vue";
 const value = ref("");
 console.log(value, "value");
+
+const save = () => {
+  console.log("xxxxx");
+};
 </script>
 <template>
   <div>
-    <mavon-editor v-model="value" />
+    <mavon-editor v-model="value" :ishljs="true" @save="save" />
   </div>
 </template>
 
